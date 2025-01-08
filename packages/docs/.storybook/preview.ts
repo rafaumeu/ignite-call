c5861dedcdbec18a711f3744c643e9b28d2a0288
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-08 07:27:11 -0300

    feat(storybook): update configuration and package dependencies for MDX support - Update .storybook/main.ts to resolve MDX indexing issues. - Update .storybook/preview.ts to include necessary imports and configurations. - Update package.json to reflect the changes.

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,4 +1,5 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from "@storybook/react"
+import { themes } from "@storybook/theming"
 
 const preview: Preview = {
   parameters: {
@@ -7,6 +8,16 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: "dark",
+      values: [
+        { name: "light", value: "#ffffff" },
+        { name: "dark", value: "#333333" },
+      ],
+    },
+    docs: {
+      theme: themes.dark, // Define o tema dark para a documentação
     },
   },
 };
