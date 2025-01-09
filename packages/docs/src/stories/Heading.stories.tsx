40410bf416e98b516b011b056cd46ac12b828265
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-09 16:34:48 -0300

    feat(components): add Heading component
    - Add new Heading component and related stories
    - Export Heading component in index.tsx

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,31 @@
+import { Heading, HeadingProps as OriginalHeadingProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+interface HeadingProps extends OriginalHeadingProps {
+  children?: React.ReactNode
+}
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default, the heading component renders a `h2` element, but you can change this by using the `as` prop.',
+      },
+    },
+  },
+}
