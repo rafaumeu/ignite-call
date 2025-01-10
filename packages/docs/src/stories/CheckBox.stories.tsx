85b22f567c7d775ef0975aeab33f2836c440471f
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-10 10:12:47 -0300

    feat(react): add CheckBox component
    - Added new CheckBox component to the React package.
    - Exported CheckBox in the React package index.tsx.
    - Updated dependencies in package.json to include @radix-ui/react-checkbox.
    - Created CheckBox stories in the Docs package.

---
+++ packages/docs/src/stories/CheckBox.stories.tsx
@@ -0,0 +1,28 @@
+import {
+  BoxComponent,
+  CheckBox,
+  CheckBoxProps,
+  TextComponent,
+} from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/CheckBox',
+  component: CheckBox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <BoxComponent
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <TextComponent size="sm">Accept therms of use</TextComponent>
+        </BoxComponent>
+      )
+    },
+  ],
+} as Meta<CheckBoxProps>
+
+export const Primary: StoryObj<CheckBoxProps> = {}
